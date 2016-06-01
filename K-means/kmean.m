2036c48c26680c491339afdcc2203d5242483ca6
sigurdjs <sigurd1007@gmail.com>
2016-06-01 11:55:51 +0200

    Update kmean.m

--- K-means/kmean.m
+++ K-means/kmean.m
@@ -1,6 +1,6 @@
+%   Read image and obtain dimensions
 Im = imread('colors-2.png');
 [M,N,~] = size(Im);
-% imshow(I);
 I = reshape(Im,[],1,3);
 K = 16;
 
@@ -8,28 +8,38 @@ K = 16;
 
 mu = randi([0 255],K,3);
 disp(mu);
-%% Assign to different clusters
+
+%% Initialize matrices
 a = zeros(length(I),length(mu));
 m = zeros(length(I),3,length(mu));
 d = zeros(length(I),length(mu));
 prevsize = zeros(length(mu),1);
 cursize = ones(length(mu),1);
+
+%   Iterates until cluster converge to fixed sizes
 while(cursize ~= prevsize)
+
     prevsize = cursize;
     i = 1:length(I);
+    %   Calculates distance between cluster-centers and all vectors
     for j = 1:length(mu)
-    %     disp(bsxfun(@minus,double(I(i,:)), mu(j,:)));
         m(:,:,j) = bsxfun(@minus,double(I(i,:)), mu(j,:));
         d(:,j) = abs(sum(m(:,:,j),2)); 
     end
+    
+    %   Assigns vectors to closest cluster
     [~, index] = min(d,[],2);
     for i = 1:length(index);
         a(i,index(i)) = 1;
     end
+    
+    %   Calculates new cluster centers based on the currently assigned vectors
     n = zeros(length(mu),1);
     for j = 1:length(mu)
         indexes = find(a(:,j));
         cursize(j) = length(indexes);
+        
+        %   Reinitialize cluster if empty.   
         if isempty(indexes)
             disp('something fucked up');
             mu(j,:) = randi([0 255],3,1);
@@ -40,6 +50,8 @@ while(cursize ~= prevsize)
         end
     end
 end
+
+%   Some plotting going on here. Might produce some extra figures
 figure(1);
 j = 1;
 for i = 1:K
@@ -50,6 +62,7 @@ for i = 1:K
     subplot(2,2,j);
     imshow(im);
     j = j + 1;
+    
     if mod(i,4) == 0
         figure((i/4) + 1);
         j = 1;
